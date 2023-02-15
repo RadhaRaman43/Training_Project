ccf122107e47ea0c0420c6008d931a56d1ee8ac1
RadhaRaman43 <ramanradha0434@gmail.com>
2023-02-15 10:55:56 +0530

    first commit

---
+++ src/test/java/com/example/Telephone_Directory/TelephoneDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Telephone_Directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelephoneDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
